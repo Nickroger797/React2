ec2b284416902299ff2d11d6e1be9d6281518f17
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-02 09:27:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
+import os  
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")  # चैनल का Username (Without @)
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))  # Log Channel ID
+BOT_TOKEN = os.getenv("BOT_TOKEN")  
+API_ID = int(os.getenv("API_ID", 0))  
+API_HASH = os.getenv("API_HASH")  
+MONGO_URI = os.getenv("MONGO_URI")  
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))  
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
